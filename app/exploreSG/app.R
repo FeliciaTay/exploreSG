760cfeb2005792879239c6177218d286ef03339f
Felicia Tay <e0323765@u.nus.edu>
2022-04-14 02:03:28 +0800

    Add files

--- app/exploreSG/app.R
+++ app/exploreSG/app.R
@@ -10,20 +10,34 @@ library(dplyr)
 library(httr)
 library(geosphere)
 library(sp)
+library(sf);
+library(stringi)
 
 ui <- fluidPage(
   tabsetPanel(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "dark_mode.css"),
+      tags$style("#weather{font-weight: bold; font-family: 'Yusei Magic', sans-serif;}"),
+      tags$style("#choose{font-size: 120%;}"),
+      tags$style("#title{font-size: 150%;}")
+    ),
     tabPanel("What's nearby?", fluid = TRUE,
              sidebarLayout(
                sidebarPanel(
                  radioButtons(inputId = "choose",label="What to look for:",
-                              choices = c("Hotels","MRT stations","Parking lots")),
-                 textInput("loc_input", label = h3("Where are you near?"))
+                              choices = c("Hotels","MRT stations","Taxi stands", "Hawker centres")),
+                 textInput("loc_input", label = h3("I am  near:"))
                ),
                mainPanel(
-                 leafletOutput(outputId = "nearby")
+                 leafletOutput(outputId = "nearby"),
+                 htmlOutput(outputId = "weather")
                )
              ),
+    ),
+    tabPanel("Tourist attractions", fluid = TRUE,
+               mainPanel(
+                 plotOutput("tourist")
+               )
     ),
     tabPanel("Live data", fluid = TRUE,
              sidebarLayout(
@@ -34,8 +48,14 @@ ui <- fluidPage(
                  textOutput("text_output")
                )
              )
+    ),
+    tabPanel("Information about this app", fluid = TRUE,
+             mainPanel(
+              htmlOutput("title"),
+              htmlOutput("desc")
+             )
     )
-  ),
+  )
 )
 
 server <- function(input, output) {
@@ -86,8 +106,46 @@ server <- function(input, output) {
     distm(c(X1, Y1), c(X2, Y2), fun = distHaversine)
   }
   
+  processLongAndLat <- function(place) {
+    url<-"https://developers.onemap.sg/commonapi/search"
+    query <- list('searchVal' = as.character(place), 'returnGeom' = 'Y', 'getAddrDetails' = 'N', 'pageNum' = '1')
+    res <- GET(url, query = query, verbose())
+    list <- content(res)$results
+    if (length(content(res)$results) == 0) { 
+      c(0,0)
+    } else {
+      c(as.numeric(list[[1]]$LONGTITUDE), as.numeric(list[[1]]$LATITUDE)) # use the top result;s long and lat
+    }
+  }
+  
   output$text_output <- renderText({ 
     "You have selected this"
+  })
+  
+  output$title <- renderUI({ 
+    HTML("Welcome to ExploreSG!")
+  })
+  
+  output$desc <- renderUI({ 
+    HTML(paste("<br>", "What's nearby? tab: You can input a location using the text input box and you will be able to toggle between finding hotels, MRT stations, taxi stands or hawker centres nearby. You can find the 2hr weather forecast in that area to help you better plan your iternary:)",
+               "<br>", "! Please only input places in Singapore. The default location the map will show is at the centre of Singapore.",
+               "<br>", "<br>", "Data tab:"))
+  })
+  
+  output$weather <- renderUI({ 
+    date <- as.character(Sys.Date())
+    time <- strftime(Sys.time(), "%T")
+    url <- paste0("https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=")
+    url <- paste0(url,date,"T",time)
+    data <- fromJSON(url)
+    weather.area.raw.data <- as.data.frame(data$items$forecasts)
+    # fit in longitude, lat from another file and merge
+    data.long.lat <- read.csv("weather.csv")
+    weather.area.raw.data <- merge(weather.area.raw.data, data.long.lat, all.x=T, by="area")
+    avg_long <- findAverageCoordOfSearch()[1]
+    avg_lat <- findAverageCoordOfSearch()[2]
+    top <- head(weather.area.raw.data %>% rowwise() %>% mutate(dist = findDist(long, lat, avg_long, avg_lat)) %>% arrange(dist), 1)
+    HTML(paste(paste("Area:", "<u>", top$area, "</u>"), paste("The 2-hour weather forecast for this area:", "<u>", top$forecast, "</u>"), sep="<br/>"))
   })
   
   output$nearby <-renderLeaflet(
@@ -104,12 +162,51 @@ server <- function(input, output) {
         mrt <- head(mrt %>% rowwise() %>% mutate(dist = findDist(lng, lat, avg_long, avg_lat)) %>% arrange(dist), 5) %>% mutate(desc = paste(station_name, type))
         mrt.data <- mrt %>% select(lng, lat, desc)
         plotWithDesc(mrt.data)
-      } else {
+      } else if (input$choose == "Taxi stands") {
         taxi.stops.data <- read.csv("lta-taxi-stop-kml.csv")
         taxi.stops.data <- taxi.stops.data[,1:2]
         taxi.stops.data <- head(taxi.stops.data %>% rowwise() %>% mutate(dist = findDist(X, Y, avg_long, avg_lat)) %>% arrange(dist), 5)
         plot(taxi.stops.data)
+      } else {
+        hawker <- read.csv("hawker_data_final.csv")
+        hawker <- head(hawker %>% rowwise() %>% mutate(dist = findDist(Longitude, Latitude, avg_long, avg_lat)) %>% arrange(dist), 5)
+        colnames(hawker)[1] <- "name_of_centre"
+        hawker.plot <- hawker %>% select(Longitude, Latitude, name_of_centre)
+        plotWithDesc(hawker.plot)
       }
+    }
+  )
+  
+  output$tourist <-renderPlot(
+    {
+      tourist <- "TOURISM.kml"
+      touristData <- st_read(tourist)
+      tourist_3857 <- st_transform(touristData, 3857)
+      # Define a function to fix the bbox to be in EPSG:3857
+      ggmap_bbox <- function(map) {
+        if (!inherits(map, "ggmap")) stop("map must be a ggmap object")
+        
+        # Extract the bounding box (in lat/lon) from the ggmap to a numeric vector, and set the names to what sf::st_bbox expects:
+        map_bbox <- setNames(unlist(attr(map, "bb")), 
+                             c("ymin", "xmin", "ymax", "xmax"))
+        
+        # Convert the bbox to an sf polygon, transform it to 3857, and convert back to a bbox (convoluted, but it works)  
+        bbox_3857 <- st_bbox(st_transform(st_as_sfc(st_bbox(map_bbox, crs = 4326)), 3857))
+        
+        # Overwrite the bbox of the ggmap object with the transformed coordinates 
+        attr(map, "bb")$ll.lat <- bbox_3857["ymin"]
+        attr(map, "bb")$ll.lon <- bbox_3857["xmin"]
+        attr(map, "bb")$ur.lat <- bbox_3857["ymax"]
+        attr(map, "bb")$ur.lon <- bbox_3857["xmax"]
+        map
+      }
+      map <- get_map("singapore", maptype = "roadmap", zoom = 11, source = "google", color = "bw")
+      # Use the function:
+      map <- ggmap_bbox(map)
+      ggmap(map) + 
+        coord_sf(crs = st_crs(3857)) + # force the ggplot2 map to be in 3857
+        #transportation
+        geom_sf(data = tourist_3857, col = "#80DA83", lwd = 0.6,inherit.aes = FALSE, show.legend = FALSE)
     }
   )
 }
